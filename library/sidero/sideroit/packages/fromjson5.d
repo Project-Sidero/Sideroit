8169a331fae383820f8121c7cb9f59d9d5a72409
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2025-03-04 01:05:34 +1300

    Remove out all the the initial ideas that won't go anywhere

--- library/sidero/sideroit/packages/fromjson5.d
+++
@@ -1,7 +0,0 @@
-module sidero.sideroit.packages.fromjson5;
-import sidero.sideroit.packages.unit;
-import sidero.fileformats.json5;
-
-UnitRef load(JSONValue jsonValue) {
-    return UnitRef.init;
-}
--- library/sidero/sideroit/packages/package.d
+++
@@ -1,4 +0,0 @@
-module sidero.sideroit.packages;
-public import sidero.sideroit.packages.artifact;
-public import sidero.sideroit.packages.buildspec;
-public import sidero.sideroit.packages.unit;
